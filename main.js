04554f357fe338d6ffc6b4fdace6a99a716401a8
AndreLau11 <hectorlau.1993@gmail.com>
2022-03-08 13:12:14 -0600

    Interactions

--- main.js
+++ main.js
@@ -3,9 +3,12 @@
 const hamburguer = document.getElementById('hamburguer');
 const navBar = document.getElementById('mobile-nav');
 const closeIcon = document.getElementById('closeIcon');
-const forums = document.getElementById('forums');
 const aboutus = document.getElementById('aboutus');
-const getintouch = document.getElementById('getintouch');
+const program = document.getElementById('program');
+const join = document.getElementById('join');
+const sponsor = document.getElementById('sponsor');
+const news = document.getElementById('news');
+const campaign = document.getElementById('campaign');
 
 function closeTab() {
     navBar.classList.add('hidden');
@@ -19,6 +22,9 @@ function openTab() {
 
 hamburguer.addEventListener('click', openTab);
 closeIcon.addEventListener('click', closeTab);
-forums.addEventListener('click', closeTab);
 aboutus.addEventListener('click', closeTab);
-getintouch.addEventListener('click', closeTab);
+program.addEventListener('click', closeTab);
+join.addEventListener('click', closeTab);
+sponsor.addEventListener('click', closeTab);
+news.addEventListener('click', closeTab);
+campaign.addEventListener('click', closeTab);
